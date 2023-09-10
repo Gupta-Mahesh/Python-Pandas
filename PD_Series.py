1744279bb8bb9da3fe8576c0053ed7863c7d9dad
Mahesh Gupta <mahesh99gupta@gmail.com>
2023-09-10 20:18:12 +0530

    Pandas series functions

--- PD_Series.py
+++ PD_Series.py
@@ -31,4 +31,77 @@ print(pd.Series(steps_walk))
 #repeat series
 print("Repeate")
 print(pd.Series(17).repeat(3)) #same index shows for all repeated element
-print(pd.Series(17).repeat(5).reset_index(drop=True))
+print(pd.Series(17).repeat(5).reset_index(drop=True)) 
+
+
+#accessing the date by index number
+data_numpy = np.random.randint(10,50,6)
+data_index = pd.Series(data_numpy)
+print("Accessing data by using index number data_index[index-value]")
+print(data_index[5])
+
+print("Accessing data by using index slicing data_index[start : end]")
+print(data_index[3:8])
+
+print("Accessing data by using index slicing data_index[start : - end]")
+print(data_index[3:-2])
+
+
+#Aggregate function 
+print("Aggregate function")
+print(data_index.agg([min,max,sum]))
+print(pd.Series.min(data_index))
+print(pd.Series.max(data_index))
+print(pd.Series.sum(data_index))
+
+
+#Series absolute function
+print("Absolute function")
+print(data_index.abs())
+
+
+# Series between function
+sr1 = pd.Series([1,2,3,4,5])
+print("Between the series")
+print(sr1.between(2,5))
+
+# String function in series
+str_series = pd.Series(["Mahesh ", "ritvika  ", "hridyansh", "  Jadugar"])
+
+##Upper and Lower Function
+
+print("Upper and lower case")
+print("Lower function:")
+print(str_series.str.lower())
+print("Upper function:")
+print(str_series.str.upper())
+
+print("length of array of each element")
+for i in str_series:
+    print(i,len(i))
+
+##Strip function which will remove unwanted spaces
+str_series = str_series.str.strip()
+print("Length after strip function")
+for i in str_series:
+    print(i,len(i))
+
+
+##Split function
+str_series1 = pd.Series(["Rain@is@happening","I'm in Hyderabad","rain happening from morning"])
+print("Split function")
+print(str_series1.str.split("@"))
+
+print("Replace function")
+print(str_series1.str.replace("@"," "))
+
+#return boolean value
+
+print("Startwith()",str_series1.str.startswith("I"))
+print("Endwith()",str_series1.str.endswith("g"))
+print("find()",str_series1.str.find("rain"))
+
+
+#convert pd to list
+
+print("Pandas series to List",str_series1.to_list())
